49a05fce962a6fb22497fd7a034e42402e5029b0
syooooring <syooooring@gmail.com>
2020-01-23 16:08:06 +0900

    csrf_post

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,11 +7,22 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index()
-    {
-        $data = [
-                'msg'=>'これはbladeを利用したサンプルです',
-        ];
-        return view('hello.index',  $data);
-    }
+  
+   public function index()
+   {
+       $data = [
+           'msg'=>'お名前を入力下さい。',
+       ];
+       return view('hello.index', $data);
+   }
+
+   public function post(Request $request)
+   {
+       $msg = $request->msg;
+       $data = [
+           'msg'=>'こんにちは、' . $msg . 'さん！',
+       ];
+       return view('hello.index', $data);
+   }
+
 }
