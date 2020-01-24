884c0b12fb3f4c7ed66334b882114480a32e3f67
syooooring <syooooring@gmail.com>
2020-01-24 17:56:51 +0900

    view_controller_fix

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,6 +10,6 @@ class HelloController extends Controller
   
    public function index()
   {
-      return view('hello.index',['message'=>'Hello!']);
+      return view('hello.index',['data'=>$request->data]);
   }
 }
