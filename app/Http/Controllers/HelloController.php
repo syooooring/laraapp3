a248e6d72acbd8a1b94bdc290a8536fbe01f56e8
syooooring <syooooring@gmail.com>
2020-01-24 16:29:59 +0900

    view_composer

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,11 +10,6 @@ class HelloController extends Controller
   
    public function index()
   {
-      $data = [
-          ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
-          ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
-          ['name'=>'山田太郎', 'mail'=>'taro@yamada']
-      ];
-      return view('hello.index',['data'=>$data]);
+      return view('hello.index',['message'=>'Hello!']);
   }
 }
