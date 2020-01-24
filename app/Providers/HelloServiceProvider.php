f74d4cac337d9b553f5219f243e7c30b63c5b2b3
syooooring <syooooring@gmail.com>
2020-01-24 16:23:26 +0900

    helloserviceprovider

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -1,28 +1,18 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+   public function boot()
+   {
+       View::composer(
+           'hello.index', function($view){
+               $view->with('view_message', 'composer message!');
+           }
+       );
+   }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
 }
