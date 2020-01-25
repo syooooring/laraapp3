811dd4e481db0b9e454a9626f88c22d3eb7e5a3e
syooooring <syooooring@gmail.com>
2020-01-25 19:27:16 +0900

    response

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -6,21 +6,16 @@ use Closure;
 
 class HelloMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $data = [
-            ['name'=>'taro', 'mail'=>'taro@yamada'],
-            ['name'=>'taro', 'mail'=>'taro@yamada'],
-            ['name'=>'taro', 'mail'=>'taro@yamada'],
-        ];
-        $request->merge(['data'=>$data]);
-        return $next($request);
-    }
+   public function handle($request, Closure $next)
+   {
+       $response = $next($request);
+       $content = $response->content();
+
+       $pattern = '/<middleware>(.*)<\/middleware>/i';
+       $replace = '<a href="http://$1">$1</a>';
+       $content = preg_replace($pattern, $replace, $content);
+
+       $response->setContent($content);
+       return $response;
+   }
 }
