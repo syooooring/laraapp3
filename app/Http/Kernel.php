d3bb62617d6702a5d63483180140afe7beb2c805
syooooring <syooooring@gmail.com>
2020-01-25 19:58:03 +0900

    middleware_groupe

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        
+        'helo' => [
+            \App\Http\Middleware\HelloMiddleware::class,
+         ],
     ];
 
     /**
