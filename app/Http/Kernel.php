076030b1b7e5121c96ce90e39ed0b1c7b2fb76ea
syooooring <syooooring@gmail.com>
2020-01-25 20:38:35 +0900

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,8 +42,8 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        
-        'helo' => [
+
+        'hello' => [
             \App\Http\Middleware\HelloMiddleware::class,
          ],
     ];
