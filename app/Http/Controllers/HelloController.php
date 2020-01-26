e8cc7ec5eca8a778ea28a39bd112c8a865938b79
syooooring <syooooring@gmail.com>
2020-01-26 17:36:29 +0900

    controller_fix

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
@@ -15,12 +16,6 @@ class HelloController extends Controller
 
   public function post(Request $request)
   {
-      $validate_rule = [
-          'name' => 'required',
-          'mail' => 'email',
-          'age' => 'numeric|between:0,150',
-      ];
-      $this->validate($request, $validate_rule);
       return view('hello.index', ['msg'=>'正しく入力されました']);
   }
 }
