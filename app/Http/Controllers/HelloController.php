9d54a5922729a7da30f7b9f9b83b83224332538f
syooooring <syooooring@gmail.com>
2020-01-27 13:01:42 +0900

    error_message_customize

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -26,16 +26,24 @@ class HelloController extends Controller
 
 public function post(Request $request)
 {
-$validator = Validator::make($request->all(), [
-    'name' => 'required',
-    'mail' => 'email',
-    'age' => 'numeric|between:0,150',
-]);
-if ($validator->fails()) {
-    return redirect('/hello')
-                ->withErrors($validator)
-                ->withInput();
+   $rules = [
+       'name' => 'required',
+       'mail' => 'email',
+       'age' => 'numeric|between:0,150',
+   ];
+   $messages = [
+       'name.required' => '名前は必ず入力して下さい。',
+       'mail.email'  => 'メールアドレスが必要です。',
+       'age.numeric' => '年齢を整数で記入下さい。',
+       'age.between' => '年齢は０～150の間で入力下さい。',
+   ];
+   $validator = Validator::make($request->all(), $rules, $messages);
+   if ($validator->fails()) {
+       return redirect('/hello')
+           ->withErrors($validator)
+           ->withInput();
+   }
+   return view('hello.index', ['msg'=>'正しく入力されました！']);
 }
-return view('hello.index', ['msg'=>'正しく入力されました！']);
-}
+
 }
