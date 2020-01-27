53f6555cb206951f71a3845c5f9753181710273e
syooooring <syooooring@gmail.com>
2020-01-27 12:53:26 +0900

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,37 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
-use Validate;
+use Validator;
 
 class HelloController extends Controller
 {
   
     public function index(Request $request)
-    {
-       $validator = Validator::make($request->query(), [
-           'id' => 'required',
-           'pass' => 'required',
-       ]);
-       if ($validator->fails()) {
-           $msg = 'クエリーに問題があります。';
-       } else {
-           $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
-       }
-       return view('hello.index', ['msg'=>$msg, ]);
-    }
+{
+   $validator = Validator::make($request->query(), [
+       'id' => 'required',
+       'pass' => 'required',
+   ]);
+   if ($validator->fails()) {
+       $msg = 'クエリーに問題があります。';
+   } else {
+       $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
+   }
+   return view('hello.index', ['msg'=>$msg, ]);
+}
 
-  public function post(Request $request)
-  {
-      $validator = Validator::make($request->all(),[
-          'name' => 'required',
-          'mail' => 'email',
-          'age' => 'numeric|between:0,150',
-      ]);
-      if ($validator->fails()) {
-          return redirect('/hello')
-          ->withErrors($validator)
-          ->withInput();
-      }
-      return view('hello.index', ['msg'=>'正しく入力されました']);
-  }
+public function post(Request $request)
+{
+$validator = Validator::make($request->all(), [
+    'name' => 'required',
+    'mail' => 'email',
+    'age' => 'numeric|between:0,150',
+]);
+if ($validator->fails()) {
+    return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+}
+return view('hello.index', ['msg'=>'正しく入力されました！']);
+}
 }
