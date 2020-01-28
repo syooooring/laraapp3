36b447acdac26897c0ccc80128fb9c649bb1b33a
syooooring <syooooring@gmail.com>
2020-01-28 17:50:58 +0900

    sqlite->mysql

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,21 +8,14 @@
 @endsection
 
 @section('content')
-   <p>{{$msg}}</p>
-   @if (count($errors) > 0)
-   <p>入力に問題があります。再入力して下さい。</p>
-   @endif
-   <table>
-   <form action="/hello" method="post">
-       {{ csrf_field() }}
-       @if ($errors->has('msg'))
-       <tr><th>ERROR</th><td>{{$errors->first('msg')}}</td></tr>
-       @endif
-       <tr><th>Message: </th><td><input type="text" name="msg"
-           value="{{old('msg')}}"></td></tr>
-       <tr><th></th><td><input type="submit" value="send"></td></tr>
-   </form>
-   </table>
+ <table>
+ <tr><th>Name</th></tr>
+ @foreach ($items as $item)
+      <tr>
+          <td>{{$item->name}}</td>
+      </tr>
+ @endforeach
+ </table>
 @endsection
 
 @section('footer')
