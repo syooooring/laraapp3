c79a9b3695421fc0548bb178b043d391a498bcc8
syooooring <syooooring@gmail.com>
2020-01-30 17:57:00 +0900

    Update PersonController.php

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $items = Person::all();
+        return view('person.index', ['items' => $items]);
+    }
 }
