19b7d9e55c86a4a5b3034cb7269f10337721d52b
syooooring <syooooring@gmail.com>
2020-01-30 20:34:53 +0900

    model_fix

--- app/Person.php
+++ app/Person.php
@@ -2,14 +2,25 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
+    
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
+
 
     public function scopeNameEqual($query, $str)
     {
@@ -24,5 +35,12 @@ class Person extends Model
     public function scopeAgeLessThan($query, $n)
     {
         return $query->where('age', '<=', $n);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new ScopePerson);
     }
 }
