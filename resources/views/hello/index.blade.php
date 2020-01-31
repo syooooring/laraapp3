48b88e35174f8f8e6bbadd7585bddf9fa99fa31b
syooooring <syooooring@gmail.com>
2020-01-31 20:06:08 +0900

    pagination

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.helloapp')
-
+<style>
+   .pagination { font-size:10pt; }
+   .pagination li { display:inline-block }
+</style>
 @section('title', 'Index')
 
 @section('menubar')
@@ -8,16 +11,17 @@
 @endsection
 
 @section('content')
-  <table>
-  <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-  @foreach ($items as $item)
-        <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->mail}}</td>
-            <td>{{$item->age}}</td>
-        </tr>
-  @endforeach
-  </table>
+   <table>
+   <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+   @foreach ($items as $item)
+       <tr>
+           <td>{{$item->name}}</td>
+           <td>{{$item->mail}}</td>
+           <td>{{$item->age}}</td>
+       </tr>
+   @endforeach
+   </table>
+   {{ $items->links() }}
 @endsection
 
 @section('footer')
